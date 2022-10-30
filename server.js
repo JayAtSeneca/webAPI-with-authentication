070a02a9013160c0f0cbccec0098abea188dea6a
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-30 02:34:35 -0400

    securing the vehicle data with user authentication using JWT and Passport

--- server.js
+++ server.js
@@ -1,15 +1,59 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
 const dataService = require("./data-service.js");
 const userService = require("./user-service.js");
 const app = express();
 
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+
+// IMPORTANT - this secret should be a long, unguessable string
+// (ideally stored in a "protected storage" area on the web server).
+// We suggest that you generate a random 50-character string
+// using the following online tool:
+// https://lastpass.com/generatepassword.php
+
+jwtOptions.secretOrKey = process.env.SECRET_OR_KEY;
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+
+  if (jwt_payload) {
+    // The following will ensure that all routes using
+    // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
+    // that matches the request payload data
+    next(null, {
+      _id: jwt_payload._id,
+      userName: jwt_payload.userName,
+      fullName: jwt_payload.fullName,
+      role: jwt_payload.role,
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+
 app.use(express.json());
 app.use(cors());
+// tell passport to use our "strategy"
+passport.use(strategy);
+// add passport as application-level middleware
+app.use(passport.initialize());
+
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/api/vehicles", (req, res) => {
+app.get("/api/vehicles", passport.authenticate('jwt', { session: false }), (req, res) => {
   dataService
     .getAllVehicles()
     .then((data) => {
@@ -33,13 +77,24 @@ app.post("/api/register", (req, res) => {
 });
 
 app.post("/api/login", (req, res) => {
-  userService
+
+    
+
+    userService
     .checkUser(req.body)
     .then((user) => {
-      res.json({ message: "login successful" });
+      let payload = {
+        _id: user._id,
+        userName: user.userName,
+        fullName: user.fullName,
+        role: user.role,
+      };
+      
+      let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({ message: 'login successful', token: token });
     })
     .catch((msg) => {
-      res.status(422).json({ message: msg });
+        res.status(422).json({ message: msg });
     });
 });
 
